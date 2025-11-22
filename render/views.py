77cf5d2c2324850c18009b41945c53fe02e14bec
Jesús Rostro <118336994+xZloy@users.noreply.github.com>
2025-11-22 13:20:21 -0600

    xd

--- render/views.py
+++ render/views.py
@@ -5,7 +5,7 @@ import requests
 from django.core.paginator import Paginator
 from django.shortcuts import render
 import pandas as pd
-from .ml.ml_utils import predict_next_consumption  # <- usa el TFLite
+from .ml.ml_utils import predict_next_consumption_from_df  
 
 API_BASE = "https://iottinaco.onrender.com"
 
@@ -32,29 +32,24 @@ def readings_chart(request):
     return render(request, "render/readings_chart.html", {"readings_json": json.dumps(data)})
 
 def _iso_to_dt(s: str):
-    # convierte "2025-11-12T03:03:53.683329Z" a datetime
+    
     return datetime.fromisoformat(s.replace("Z", "+00:00"))
 
 def predict_view(request):
     prediction = None
     try:
-        # 1) OBTENER TODAS LAS LECTURAS EXISTENTES
         url = "https://iottinaco.onrender.com/readings/all"
         data = requests.get(url).json()
 
-        # 2) Convertir a DataFrame
         df = pd.DataFrame(data)
 
-        # 3) Mantener solo columnas necesarias
         cols = ["ts", "flow_lpm", "water_temp_c", "humidity_pct"]
         for c in cols:
             if c not in df.columns:
                 df[c] = 0
 
-        # 4) Quedarse con las ÚLTIMAS 3 lecturas
         df = df.tail(3)
 
-        # 5) Enviar al predictor
         prediction = predict_next_consumption_from_df(df)
 
     except Exception as e:
